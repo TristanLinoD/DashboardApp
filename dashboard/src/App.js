fa59d0d03c9e468bfa67173acfc883e64f2e6b0b
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2022-07-11 20:44:48 -0500

    We create the structure of the sidebar with the navbar in App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,20 +6,71 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import './App.css'
 
 const App = () => {
-  return (
-    <div>
-        <BrowserRouter>
-            <div className='flex relative dark:bg-main-dark-bg'>
-                <div className='fixed right-4 bottom-4' style={{ zIndex:'1000'}}>
-                    <TooltipComponent content='Settings' position='Top'>
-                        <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background:'blue', borderRadius:'50%'}}>
-                            <FiSettings />
-                        </button>
-                    </TooltipComponent>
+    const activeMenu = true;
+    return (
+        <div>
+            <BrowserRouter>
+                {/* Button settings  */}
+                <div className='flex relative dark:bg-main-dark-bg'>
+                    <div className='fixed right-4 bottom-4' style={{ zIndex:'1000'}}>
+                        <TooltipComponent content='Settings' position='Top'>
+                            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background:'blue', borderRadius:'50%'}}>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
                 </div>
-            </div>
-        </BrowserRouter>
-    </div>
+
+                {/* Sidebar  */}
+                <div>
+                    {activeMenu ? (
+                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                            Sidebar
+                        </div>   
+                    ): (
+                        <div className='w-0 dark:bg-secondary-dark-bg'>
+                            Sidebar w-0
+                        </div>    
+                    )}
+                    <div className={
+                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72':'flex-2'}`
+                    }>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg'>
+                            Navbar
+                        </div>
+                    </div>
+                    <div>
+                        <Routes>
+                            {/* dashboard  */}
+                            <Route path="/" element="Ecommerce"/>
+                            <Route path="/ecommerce" element="Ecommerce"/>
+
+                            {/* pages  */}
+                            <Route path="/orders" element="Orders"/>
+                            <Route path="/employees" element="Employees"/>
+                            <Route path="/customers" element="Customer" />
+
+                            {/* apps  */}
+                            <Route path="/kanban" element="Kanban"/>
+                            <Route path="/editor" element="Editor"/>
+                            <Route path="/calendar" element="Calendar"/>
+                            <Route path="/color-picker" element="ColorPicker"/>
+
+                            {/* charts  */}
+                            <Route path="/line" element="Line"/>
+                            <Route path="/area" element="Area"/>
+                            <Route path="/bar" element="Bar"/>
+                            <Route path="/pie" element="Pie"/>
+                            <Route path="/financial" element="Financial"/>
+                            <Route path="/color-mapping" element="ColorMapping"/>
+                            <Route path="/pyramid" element="Pyramid"/>
+                            <Route path="/stacked" element="Stacked"/>
+
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
   )
 }
 
