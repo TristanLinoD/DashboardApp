a8cfc29413f0eb8b16db5dbf8d389ffb0ac7f720
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2022-07-15 12:30:04 -0500

    We create the navbar with the handlers of the sidebar

--- dashboard/src/contexts/ContextProvider.js
+++ dashboard/src/contexts/ContextProvider.js
@@ -8,18 +8,26 @@ const StateContext = createContext();
 // The option is open o close, we initial all close
 
 const initialState = {
-  chat: false,
   cart: false,
+  chat: false,
   userProfile: false,
   notification: false,
 };
 
 // La propiedad children nos ayuda a pasar contenido, elementos, componentes 
 // <App> Contenido <h1> Children </h1> <Componente 1 /> </App>
-// You always return children because wherever wrapper the content will be display  
+
 export const ContextProvider = ({children}) => {
+  // States
   const [activeMenu, setActiveMenu] = useState(true);
-  
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  // Handled click
+  const handleClick = (clicked) => {
+    setIsClicked({...initialState, [clicked]: true});
+  }
+
   return (
     <StateContext.Provider
       // Wherever value you pass through here, they will be pass for all the components inside of emtire aplicatition 
@@ -27,8 +35,14 @@ export const ContextProvider = ({children}) => {
       value={{ 
         activeMenu,
         setActiveMenu,
+        isClicked, 
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
       }}
     >
+      {/* You always return children because wherever wrapper the content will be display   */}
       {children}
     </StateContext.Provider>
   );
