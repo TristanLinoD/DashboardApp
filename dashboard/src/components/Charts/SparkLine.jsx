7f6b3fbf620f9721c83689f09bb7fbd90f731d04
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2022-07-16 20:34:44 -0500

    We create the ecommerce page with some bugs in the charts (sparkline and barchart), when you change the screen size sometimes it dont render the page, some problem with the component SparkLine

--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
-  return (
-    <div>SparkLine</div>
-  )
+// WHY IS NOT WORKING WITH FUNCTIONS
+// const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
+//   return (
+//     <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: 2}} dataSource={data} xName="x" yName='y' type={type}>
+//       {/* <Inject services={[SparklineTooltip]} /> */}
+//     </SparklineComponent>
+//   );
+// };
+class SparkLine extends React.Component {
+
+  render() {
+    return (<SparklineComponent id={this.props.id}
+      height={this.props.height}
+      width={this.props.width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={this.props.color}
+      border={{ color: this.props.currentColor, width: 2 }}
+      dataSource={this.props.data}
+      xName="x"
+      yName="yval"
+      type={this.props.type}
+      tooltipSettings={{visible: true, format: '${x} : data ${yval}', trackLineSettings: {visible: true}}}
+      >
+    <Inject services={[SparklineTooltip]}/>
+</SparklineComponent>);
+  }
 }
 
 export default SparkLine
