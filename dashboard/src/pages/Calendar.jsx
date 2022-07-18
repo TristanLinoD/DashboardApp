a5de420b4d1c56a0b9499872b43b6e939ff8e77d
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2022-07-18 14:34:36 -0500

    Calendar app

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className='p-2 m-2 md:p-10 md:m-10 bg-white rounded-3xl mt-24'>
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent 
+        height="650px"
+        eventSettings={{dataSource: scheduleData}}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
   )
 }
 
