3207df5654caaf70e4c02c55d721907b6218870a
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2022-07-21 10:58:13 -0500

    Bug fix Sparkline component nad we create two pages with component Line, Area Chart

--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -9,24 +9,56 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 //     </SparklineComponent>
 //   );
 // };
-class SparkLine extends React.Component {
+// class SparkLine extends React.Component {
 
+//   render() {
+//     return (<SparklineComponent id={this.props.id}
+//       height={this.props.height}
+//       width={this.props.width}
+//       lineWidth={1}
+//       valueType="Numeric"
+//       fill={this.props.color}
+//       border={{ color: this.props.currentColor, width: 2 }}
+//       dataSource={this.props.data}
+//       xName="x"
+//       yName="yval"
+//       type={this.props.type}
+//       tooltipSettings={{visible: true, format: '${x} : data ${yval}', trackLineSettings: {visible: true}}}
+//       >
+//     <Inject services={[SparklineTooltip]}/>
+// </SparklineComponent>);
+//   }
+// }
+class SparkLine extends React.PureComponent {
   render() {
-    return (<SparklineComponent id={this.props.id}
-      height={this.props.height}
-      width={this.props.width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={this.props.color}
-      border={{ color: this.props.currentColor, width: 2 }}
-      dataSource={this.props.data}
-      xName="x"
-      yName="yval"
-      type={this.props.type}
-      tooltipSettings={{visible: true, format: '${x} : data ${yval}', trackLineSettings: {visible: true}}}
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
       >
-    <Inject services={[SparklineTooltip]}/>
-</SparklineComponent>);
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
   }
 }
 
